4211acf2f1f22ba1cca62bf4fc9d2c8b357431d7
Arun Drelich <ad2476@gmail.com>
2012-03-26 18:05:24 -0400

    Elaborated the Compound class

--- Chemistry.h
+++ Chemistry.h
@@ -6,20 +6,23 @@
 #include <string>
 #include <algorithm>
 
+#define CLEARSCREEN "clear"
+
 using namespace std;
 
 class Compound {
 
 	vector<int> v_Elements; // container of each element, which will use atomic number instead of name
-	int elements; // number of elements in the molecule
-	vector<int> quantities; // quantity of each element in the compound, in order
-	string molecule; // Raw input containing the formula
-
+	int i_elements; // number of elements in the molecule
+	vector<int> v_Quantities; // quantity of each element in the compound, in order
+	float f_mass; // Mass of compound
+	vector<float> v_Composition; // container of each element's percent comp
+	
 	public:
-	Molecule();
+	Molecule(string s_molecule);
 	void parseString();
-	float findMass();
-	float percentComp();
+	void findMass();
+	void percentComp();
 	float percentYield();
 };
 
