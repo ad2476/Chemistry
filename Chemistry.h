0b0658f9efd54b2da2ca27a4ee0997e42ced62f9
ad2476 <ad2476@gmail.com>
2012-03-26 11:10:49 -0300

    Created a basic class Compound

--- Chemistry.h
+++ Chemistry.h
@@ -10,6 +10,17 @@ using namespace std;
 
 class Compound {
 
-}
+	vector<int> v_Elements; // container of each element, which will use atomic number instead of name
+	int elements; // number of elements in the molecule
+	vector<int> quantities; // quantity of each element in the compound, in order
+	string molecule; // Raw input containing the formula
+
+	public:
+	Molecule();
+	void parseString();
+	float findMass();
+	float percentComp();
+	float percentYield();
+};
 
 #endif
