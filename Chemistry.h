44d018657bb5d7c5e08dc86610611ef37bd00e38
Arun Drelich <ad2476@gmail.com>
2012-03-25 23:33:18 -0400

    Created header file to store includes and classes

---
+++ Chemistry.h
@@ -0,0 +1,15 @@
+#ifndef _CHEMISTRY_H_
+	#define _CHEMISTRY_H_
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Compound {
+
+}
+
+#endif
