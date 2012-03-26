d6b0c3eb51f1e80e4c095bc064facb873cc19f88
Arun Drelich <ad2476@gmail.com>
2012-03-25 21:15:10 -0400

    Added some includes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+// Probably gonna make a separate header file for some things, like class structures
 
 using namespace std;
 
 int main()
 {
+	
 	cout << "Hello World!" << endl;
 	return 0;
 }
