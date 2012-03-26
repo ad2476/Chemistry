c01d5598ea31e97490e804aa1e5fb911ef4a5a06
Arun Drelich <ad2476@gmail.com>
2012-03-25 22:47:30 -0400

    Added a basic welcome screen. To be developed.

--- main.cpp
+++ main.cpp
@@ -3,13 +3,20 @@
 #include <string>
 #include <algorithm>
 
-// Probably gonna make a separate header file for some things, like class structures
-
 using namespace std;
 
 int main()
 {
+	char choice;
 	
 	cout << "Welcome to Chemistry!" << endl;
+	cout << "---------------------" << endl;
+	cout << "A) Calculate molar mass\n";
+	cout << "B) Calculate percent composition\n";
+	cout << "C) Calculate percent yield" << endl;
+	
+	cout << "> ";
+	cin >> choice;
+	
 	return 0;
 }
