69340e9d2248e4e33c5f1915d19b9c42793b01ef
Arun Drelich <ad2476@gmail.com>
2012-03-26 21:39:38 -0400

    Created unordered map of Elements and their corresponding atomic number.

--- Chemistry.h
+++ Chemistry.h
@@ -12,19 +12,64 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
+#include <locale>
 
 #define CLEARSCREEN "clear"
 
 using namespace std;
 
-enum ATOMIC_NUMBER { "H"=1, "Li"=3, "Be", "B", "C", "N", "O", "F", "Na"=11, "Mg", "Al", "Si", "P", "S", "Cl",
-	"K"=19, "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br",
-	"Rb"=37, "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs",
-	"Ba", "Os"=76, "Ir", "Pt", "Au", "Hg", "Tl", "Pb" 
-}; // This isn't right, I know. How should I store something like this...?
+unordered_map<string, int> ATOMIC_NUMBER;
+ATOMIC_NUMBER["H"]=1;
+ATOMIC_NUMBER["Li"]=3;
+ATOMIC_NUMBER["Be"]=4;
+ATOMIC_NUMBER["B"]=5;
+ATOMIC_NUMBER["C"]=6;
+ATOMIC_NUMBER["N"]=7;
+ATOMIC_NUMBER["O"]=8;
+ATOMIC_NUMBER["F"]=9;
+ATOMIC_NUMBER["Na"]=11;
+ATOMIC_NUMBER["Mg"]=12;
+ATOMIC_NUMBER["Al"]=13;
+ATOMIC_NUMBER["Si"]=14;
+ATOMIC_NUMBER["P"]=15;
+ATOMIC_NUMBER["S"]=16;
+ATOMIC_NUMBER["Cl"]=17;
+ATOMIC_NUMBER["K"]=19;
+ATOMIC_NUMBER["Ca"]=20;
+ATOMIC_NUMBER["Sc"]=21;
+ATOMIC_NUMBER["Ti"]=22;
+ATOMIC_NUMBER["Cr"]=24;
+ATOMIC_NUMBER["Mn"]=25;
+ATOMIC_NUMBER["Fe"]=26;
+ATOMIC_NUMBER["Co"]=27;
+ATOMIC_NUMBER["Ni"]=28;
+ATOMIC_NUMBER["Cu"]=29;
+ATOMIC_NUMBER["Zn"]=30;
+ATOMIC_NUMBER["Ga"]=31;
+ATOMIC_NUMBER["Ge"]=32;
+ATOMIC_NUMBER["As"]=33;
+ATOMIC_NUMBER["Se"]=34;
+ATOMIC_NUMBER["Br"]=35;
+ATOMIC_NUMBER["Rb"]=37;
+ATOMIC_NUMBER["Sr"]=38;
+ATOMIC_NUMBER["Zr"]=40;
+ATOMIC_NUMBER["Ag"]=47;
+ATOMIC_NUMBER["Cd"]=48;
+ATOMIC_NUMBER["In"]=49;
+ATOMIC_NUMBER["Sn"]=50;
+ATOMIC_NUMBER["Sb"]=51;
+ATOMIC_NUMBER["I"]=53;
+ATOMIC_NUMBER["Xe"]=54;
+ATOMIC_NUMBER["Cs"]=55;
+ATOMIC_NUMBER["Ba"]=56;
+ATOMIC_NUMBER["Os"]=76;
+ATOMIC_NUMBER["Pt"]=78;
+ATOMIC_NUMBER["Au"]=79;
+ATOMIC_NUMBER["Hg"]=80;
+ATOMIC_NUMBER["Pb"]=82;
 
-enum ATOMIC_MASS { // Same problem here
-};
+
 
 class Compound {
 
